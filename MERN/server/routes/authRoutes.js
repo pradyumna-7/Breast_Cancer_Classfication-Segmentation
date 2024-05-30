d50c1546e33cb78a26f91680096691b3c12d9d08
Pradyumna_J <64280581+pradyumna-7@users.noreply.github.com>
2024-05-30 16:08:08 +0530

    Update authRoutes.js

--- MERN/server/routes/authRoutes.js
+++ MERN/server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-const { registerUser, loginUser, createStudent, getStudents, findStudent, updateStudent, deleteStudent, deleteAll, verified, sendOTP, verifyOTP} = require('../controllers/authControllers')
+const { registerUser, loginUser, createStudent, getStudents, findStudent, updateStudent, deleteStudent, deleteAll, verified} = require('../controllers/authControllers')
 
 //middleware
 router.use(cors(
@@ -16,12 +16,8 @@ router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/create', createStudent)
 router.get('/students', getStudents)
-router.get('/getstudent/:id', findStudent)
-router.put('/update/:id', updateStudent)
-router.delete('/delete/:id', deleteStudent)
-router.delete('/deleteall', deleteAll)
 router.put('/verify/:id', verified)
-router.post('/sendotp', sendOTP)
-router.post('/verifyotp', verifyOTP)
+// router.post('/sendotp', sendOTP)
+// router.post('/verifyotp', verifyOTP)
 // router.post('/upload-image', uploadImage)
 module.exports = router 
